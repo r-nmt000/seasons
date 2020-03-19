f0a296d907e25f0ca7b9a036db40f5ee652ddda5
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-19 17:59:48 +0900

    Use helper method to render common part of screen

--- src/index.js
+++ src/index.js
@@ -16,7 +16,8 @@ class App extends React.Component {
             }
         );
     }
-    render() {
+
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -24,6 +25,14 @@ class App extends React.Component {
             return <SeasonDisplay lat={this.state.lat}/>
         }
         return <Spinner message="Please accept location request"/>;
+    }
+
+    render() {
+        return (
+            <div className="boarder red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
